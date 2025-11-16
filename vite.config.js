1b926db938c566c26a09736b297845696d494e8c
dpovi <dpovi2458@gmail.com>
2025-11-16 14:20:18 -0500

    Fix deployment config

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: 8080
+  },
+  build: {
+    outDir: 'dist'
+  }
 })
