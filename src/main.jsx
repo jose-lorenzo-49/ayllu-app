9bed5c9013bbc58588b71a1edbe773a9551e4c5e
dpovi <dpovi2458@gmail.com>
2025-11-16 14:43:06 -0500

    Connect to Supabase backend

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import AylluIntegrado from './AylluIntegrado'
+import AylluSupabase from './AylluSupabase'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AylluIntegrado />
+    <AylluSupabase />
   </React.StrictMode>,
 )
